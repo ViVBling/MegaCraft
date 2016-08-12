3d05669720d69a836598930dcc4c269a989ee8bf
Vivian Aktjef <vivian-aktjef@t-online.de>
2016-08-12 14:13:28 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,121 @@
+var CONFIG = {
+  "maps": {
+    "world_isometric_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1470954325,
+        0,
+        1470954325,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "MegaCraft Server - Day - 3D",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": 192,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "world_isometric_night": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1470954325,
+        0,
+        1470954325,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "MegaCraft Server - Night - 3D",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": 192,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "world_topdown_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1470954325,
+        0,
+        1470954325,
+        0
+      ],
+      "maxZoom": 4,
+      "name": "MegaCraft Server - Day - 2D",
+      "renderView": "topdown",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "world_topdown_t16",
+      "tileSize": 1536,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "world_isometric_day",
+    "world_isometric_night",
+    "world_topdown_day"
+  ],
+  "tileSetGroups": {
+    "world_isometric_t1": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "world_topdown_t16": {
+      "maxZoom": 4,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 16
+    }
+  }
+};
